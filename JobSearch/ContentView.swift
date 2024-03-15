29ca31e7ade5fb1784083268ec921fe4ab872822
Vlad <boguzhvlad@gmail.com>
2024-03-15 18:12:31 +0300

    Initial Commit

---
+++ JobSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JobSearch
+//
+//  Created by Vlad Boguzh on 15.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JobSearch/JobSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobSearchApp.swift
+//  JobSearch
+//
+//  Created by Vlad Boguzh on 15.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct JobSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
